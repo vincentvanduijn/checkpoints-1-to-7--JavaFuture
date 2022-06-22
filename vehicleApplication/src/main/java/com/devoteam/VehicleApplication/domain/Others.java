c1edd09f747e60595471ac11adf216076cf87a63
Vincent van Duijn <vincent.van.duijn@devoteam.com>
2022-06-22 15:52:06 +0200

    Fix

---
+++ vehicleApplication/src/main/java/com/devoteam/VehicleApplication/domain/Others.java
@@ -0,0 +1,9 @@
+package Devoteam.Checkpoints.vehicleApplication.src.main.java.com.devoteam.VehicleApplication.domain;
+
+public class Others extends Vehicle {
+
+    public Others(Automaker automaker, String model, String color, int year, VehicleTypeEnum vehicleType) {
+        super(automaker, model, color, year, vehicleType);
+    }
+
+}
